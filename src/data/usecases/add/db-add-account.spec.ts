bec7b971457cbb3ed45385da05a2decc11e8d829
Pedro Henrique Bruning de Oliveira Schier <pedrohboschier@gmail.com>
2022-02-24 21:42:40 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/add/db-add-account.spec.ts
+++ src/data/usecases/add/db-add-account.spec.ts
@@ -37,4 +37,16 @@ describe('Db Add Account', () => {
     await sut.add(accountData)
     expect(encryptSpy).toBeCalledWith('valid_password')
   })
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
