d001ca2b4f7ced86c139f254f38fff75657f4e6e
Pedro Henrique Bruning de Oliveira Schier <pedrohboschier@gmail.com>
2022-02-28 12:53:11 -0300

    refactor: make ServerError receive the error.stack

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -164,7 +164,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(new Error()))
   })
 
   test('Should call AddAccount with correct values', async () => {
@@ -201,7 +201,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(new Error()))
   })
 
   test('Should return 200 if valid data is provided', async () => {
--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (error: Error) {
     super('Internal Server Error')
     this.name = 'ServerParamError'
+    this.stack = error.stack
   }
 }
